4c8c9f24b7cf522c2b3711e801ab99afcf77735c
firas <fkmkh11@gmail.com>
2024-01-03 12:49:12 +0200

    build the user adderss route

---
+++ src/routes/UserRoutes.ts/UserAddressRoute.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { UserAddressController } from '../../controllers/User Management/UserAddressController';
+const router = express.Router();
+
+router.get('/addresses', UserAddressController.getUserAddresses);
+
+router.get('/addresses/:username', UserAddressController.getUserAddressByUsername);
+
+router.post('/addresses', UserAddressController.createUserAddress);
+
+router.put('/addresses', UserAddressController.updateUserAddress);
+
+
+export default router;
