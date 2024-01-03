e5db4b322a1e700cd6afe2a27171db5c96c55ca6
firas <fkmkh11@gmail.com>
2024-01-03 18:03:15 +0200

    remove file configure form controller

--- src/controllers/Product Management/ProductController.ts
+++ src/controllers/Product Management/ProductController.ts
@@ -6,20 +6,6 @@ import { Product } from '../../DataBase/entities/product';
 import { ProductCategory, productCategory } from '../../DataBase/entities/product_category';
 
 
-const router = express.Router();
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({ storage: storage });
-
-
 export class ProductController {
     static getAllProducts = async (req: express.Request, res: express.Response) => {
         try {
