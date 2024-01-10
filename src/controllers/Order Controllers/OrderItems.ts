a49c363caecf5cbd4ea4c3c9d852991e8c39c454
firas <fkmkh11@gmail.com>
2024-01-10 15:13:05 +0200

    adding order items controller

---
+++ src/controllers/Order Controllers/OrderItems.ts
@@ -0,0 +1,8 @@
+import { OrderItems } from "../../DataBase/entities/order_items";
+import { Order } from "../../DataBase/entities/order";
+import { User } from "../../DataBase/entities/user";
+import { UserPayment } from "../../DataBase/entities/user_payment";
+
+export class OrderItemsController {
+    
+}
