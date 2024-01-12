5decc456e8559d2b4291615dc3f6349bbfb8425c
firas <fkmkh11@gmail.com>
2024-01-12 20:20:11 +0200

    building discount route

--- src/routes/Discount Route/DiscountRoute.ts
+++ src/routes/Discount Route/DiscountRoute.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import DiscountController from '../../controllers/Discount controller/DiscountController';
 const route = express.Router();
 
-// Middlewares
 route.get('/discounts', DiscountController.AddDiscount);
 
 export default route;
